6a5dcac574b181d6738677e0a241753104df4e57
Nidhogg <3130242957@qq.com>
2024-04-06 17:49:30 +0800

    优化参数-O3修改为-O2

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@
 
 release_dir="./Release"
 
-if test -z "$(g++-13 -O3 -o main main.cpp)"; then
+if test -z "$(g++-13 -O2 -o main main.cpp)"; then
           mv main ${release_dir}/demo
           cd ${release_dir}
           bash ./run_simple_demo.sh
